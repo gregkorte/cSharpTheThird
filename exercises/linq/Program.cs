0091c3ccf2cb6c2c1e533f35b35ea28f53927a3a
Greg Korte <gregmkorte@gmail.com>
2018-01-06 11:49:41 -0600

    Completed Part 5: Custom Types.

--- exercises/linq/Program.cs
+++ exercises/linq/Program.cs
@@ -87,6 +87,45 @@ namespace linq
                 Console.WriteLine(num);
             };
 
+            /* 
+            Given the same customer set, display how many millionaires per bank.
+            Ref: https://stackoverflow.com/questions/7325278/group-by-in-linq
+
+            Example Output:
+            WF 2
+            BOA 1
+            FTB 1
+            CITI 1
+            */
+
+            List<Customer> customers = new List<Customer>() {
+                new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+                new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+                new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+                new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+                new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+                new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+                new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+                new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+                new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+                new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            };
+            
+            var banks =  from c in customers
+                                where c.Balance >= 1000000.00
+                                group c.Balance by c.Bank into m
+                                select new {Bank = m.Key, Count = m.Count()};
+            foreach (var b in banks) {
+                    Console.WriteLine("{0} {1}", b.Bank, b.Count);
+                }
+        }
+
+                // Build a collection of customers who are millionaires
+        public class Customer
+        {
+            public string Name { get; set; }
+            public double Balance { get; set; }
+            public string Bank { get; set; }
         }
     }   
 }
