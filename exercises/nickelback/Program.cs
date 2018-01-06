4e1a1816a5b172af60514451a5aa1f25b0ce3494
Greg Korte <gregmkorte@gmail.com>
2018-01-06 17:06:52 -0600

    Completed Nickelback exercise.

---
+++ exercises/nickelback/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nickelback
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string, string)> goodSongs = new List<(string, string)>();
+            HashSet<(string, string)> allSongs = new HashSet<(string, string)>(){
+                ("Nickelback", "Someday"),
+                ("Nickelback", "Too Bad"),
+                ("Yes", "Tempus Fugit"),
+                ("Rush", "Freewill"),
+                ("The Beatles", "Polythene Pam"),
+                ("Cat Stevens", "Year of the Cat"),
+                ("Grateful Dead", "Ripple")
+            };
+            foreach((string, string) t in allSongs){
+                if(t.Item1 != "Nickelback"){
+                    goodSongs.Add(t);
+                }
+            };
+            foreach((string, string) tup in goodSongs){
+                Console.WriteLine($"{tup.Item2} by {tup.Item1}");
+            };
+
+        }
+    }
+}
