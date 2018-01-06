6a8ad0be92042bf8525995730694d6530176d2dc
Greg Korte <gregmkorte@gmail.com>
2018-01-06 10:49:26 -0600

    Completed Part 4: Partitioning Operations.

--- exercises/linq/Program.cs
+++ exercises/linq/Program.cs
@@ -71,6 +71,21 @@ namespace linq
             };
             Console.WriteLine($"Our most expensive price is ${prices.Max()}");
             
+            /*
+            Store each number in the following List until a perfect square
+            is detected.
+
+            Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+            */
+            List<int> wheresSquaredo = new List<int>()
+            {
+                66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+            };
+
+            IEnumerable<int> notSquaredo = wheresSquaredo.TakeWhile(x => Math.Sqrt(x) % 1 != 0);
+            foreach(int num in notSquaredo){
+                Console.WriteLine(num);
+            };
 
         }
     }   
