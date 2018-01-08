af1be82269012aead8c7bdd49bc9461a6bff7a6f
Greg Korte <gregmkorte@gmail.com>
2018-01-08 12:31:15 -0600

    Completed exercise.

---
+++ exercises/random/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine(random);
+            List<int> numbers = new List<int>();
+            List<int> squares = new List<int>();
+            for(var i = 0; i < 10; i++){
+                numbers.Add(random.Next(25));
+            }
+            foreach(int number in numbers){
+                int sq = number*number;
+                squares.Add(sq);
+                if(sq % 2 != 0){
+                    squares.Remove(sq);
+                }
+            }
+            foreach(int evenSquare in squares){
+                Console.WriteLine(evenSquare + " ");
+            }
+
+        }
+    }
+}
