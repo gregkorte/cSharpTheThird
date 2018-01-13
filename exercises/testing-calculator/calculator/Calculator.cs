3225f08855ee78fb6fd658663e270db633afe3fe
Greg Korte <gregmkorte@gmail.com>
2018-01-13 14:16:13 -0600

    Setup testing calculator, two add function tests passing.

---
+++ exercises/testing-calculator/calculator/Calculator.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace calculator
+{
+    public class Calculator
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
