2332c8597656249539cc86277b27ca6eabd968f3
Greg Korte <gregmkorte@gmail.com>
2018-01-14 11:41:34 -0600

    Finished testing calculator exercise.

--- exercises/testing-calculator/calculator.tests/calculator.tests.cs
+++ exercises/testing-calculator/calculator.tests/calculator.tests.cs
@@ -30,5 +30,105 @@ namespace calculator.tests
 
             Assert.Equal(nextSum, 183);
         }
+
+        [Fact]
+        public void AddPositiveNegativeIntegers()
+        {
+            int sum = _calculator.Add(-9, 99);
+
+            Assert.Equal(sum, 90);
+        }
+
+        [Fact]
+        public void AddTwoNegativeIntegers()
+        {
+            int sum = _calculator.Add(-9, -91);
+
+            Assert.Equal(sum, -100);
+        }
+
+        [Fact]
+        public void SubtractTwoIntegers()
+        {
+            int difference = _calculator.Subtract(54, 29);
+
+            Assert.Equal(difference, 25);
+        }
+
+        [Fact]
+        public void SubtractPositiveNegativeIntegers()
+        {
+            int difference = _calculator.Subtract(-9, 91);
+
+            Assert.Equal(difference, -100);
+        }
+
+        [Fact]
+        public void SubtractTwoNegativeIntegers()
+        {
+            int difference = _calculator.Subtract(-9, -99);
+
+            Assert.Equal(difference, 90);
+        }
+
+        [Fact]
+        public void MultiplyTwoIntegers()
+        {
+            int product = _calculator.Multiply(6, 4);
+
+            Assert.Equal(product, 24);
+        }
+
+        [Fact]
+        public void MultiplyPositiveNegativeIntegers()
+        {
+            int product = _calculator.Multiply(-6, 4);
+
+            Assert.Equal(product, -24);
+        }
+
+        [Fact]
+        public void MultiplyTwoNegativeIntegers()
+        {
+            int product = _calculator.Multiply(-6, -4);
+
+            Assert.Equal(product, 24);
+        }
+
+        [Fact]
+        public void DivideTwoIntegers()
+        {
+            int quotient = _calculator.Divide(10, 5);
+
+            Assert.Equal(quotient, 2);
+        }
+
+        [Fact]
+        public void DividePositiveNegativeIntegers()
+        {
+            int quotient = _calculator.Divide(-10, 5);
+
+            Assert.Equal(quotient, -2);
+        }
+
+        [Fact]
+        public void DivideTwoNegativeIntegers()
+        {
+            int quotient = _calculator.Divide(-10, -5);
+
+            Assert.Equal(quotient, 2);
+        }
+
+        /* 
+        THIS WAS DIFFICULT TO FIND:
+        https://stackoverflow.com/questions/45017295/assert-an-exception-using-xunit
+        */
+        [Fact]
+        public void NotDivideByZero()
+        {
+            Action quotient = () => _calculator.Divide(10, 0);
+
+            Assert.Throws<DivideByZeroException>(quotient);
+        }
     }
 }
--- exercises/testing-calculator/calculator/Calculator.cs
+++ exercises/testing-calculator/calculator/Calculator.cs
@@ -8,5 +8,25 @@ namespace calculator
         {
             return num1 + num2;
         }
+
+        public int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Multiply(int num1 , int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Divide(int num1, int num2)
+        {
+            if(num2 == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            else
+            return num1 / num2;
+        }
     }
 }
