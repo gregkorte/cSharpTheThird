52fdbd128756fff1aaa6bd3c4a541dc0849eed75
Greg Korte <gregmkorte@gmail.com>
2018-01-27 10:56:04 -0600

    Added database context.

---
+++ bangazon/api/Data/ApiContext.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using api.Models;
+
+namespace MusicHistoryApi.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Computer> Computer { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<EmployeeComputer> EmployeeComputer { get; set; }
+        public DbSet<EmployeeTrainingProgram> EmployeeTrainingProgram { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderProduct> OrderProduct { get; set; }
+        public DbSet<PaymentType> PaymentType { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<ProductType> ProductType { get; set; }
+        public DbSet<TrainingProgram> TrainingProgram { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Computer>()
+                .Property(b => b.DateCreated)
+                .HasDefaultValueSql("strftime('%Y-%m-%d %H:%M:%S')");
+
+            modelBuilder.Entity<Customer>()
+                .Property(b => b.DateCreated)
+                .HasDefaultValueSql("strftime('%Y-%m-%d %H:%M:%S')");
+
+            modelBuilder.Entity<Order>()
+                .Property(b => b.DateCreated)
+                .HasDefaultValueSql("strftime('%Y-%m-%d %H:%M:%S')");
+        }
+    }
+}
