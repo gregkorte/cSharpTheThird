2d8cd239d6608ff4c1b81a89af4c4840b4613171
Greg Korte <gregmkorte@gmail.com>
2018-01-29 08:28:57 -0600

    Completed employees controller. All verbs working correctly.

---
+++ bangazon/api/Controllers/EmployeesController.cs
@@ -0,0 +1,140 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using api.Data;
+using api.Models;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public EmployeesController(ApplicationDbContext ctx)
+        {
+            _context = ctx;
+        }
+        // GET api/employees
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var employees = _context.Employee.ToList();
+            if(employees == null)
+            {
+                return NotFound();
+            }
+            return Ok(employees);
+        }
+
+        // GET api/employees/5
+        [HttpGet("{id}", Name = "GetSingleEmployee")]
+        public IActionResult Get(int id)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                Employee employee = _context.Employee.Single(c => c.EmployeeId == id);
+
+                if(employee == null)
+                {
+                    return NotFound();
+                }
+                return Ok(employee);
+            }
+            catch(System.InvalidOperationException ex)
+            {
+                Console.Write(ex);
+                return NotFound();
+            }
+        }
+
+        // POST api/employees
+        [HttpPost]
+        public IActionResult Post([FromBody]Employee employee)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _context.Employee.Add(employee);
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch(DbUpdateException)
+            {
+                if(EmployeeExists(employee.EmployeeId))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return CreatedAtRoute("GetSingleEmployee", new { id = employee.EmployeeId }, employee);
+        }
+
+        // PUT api/employees/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]Employee employee)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if(id != employee.EmployeeId)
+            {
+                return BadRequest();
+            }
+            _context.Employee.Update(employee);
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException) 
+            {
+                if(!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return new StatusCodeResult(StatusCodes.Status204NoContent);
+        }
+
+        // DELETE api/employees/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Employee employee = _context.Employee.Single(c => c.EmployeeId == id);
+
+            if(employee == null)
+            {
+                return NotFound();
+            }
+            _context.Employee.Remove(employee);
+            _context.SaveChanges();
+            return Ok(employee);
+        }
+
+        private bool EmployeeExists(int employeeId)
+        {
+            return _context.Employee.Any(c => c.EmployeeId == employeeId);
+        }
+    }
+}
