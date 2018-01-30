0682d5132335924f812351b4ca9c2816370bf084
Greg Korte <gregmkorte@gmail.com>
2018-01-30 12:35:36 -0600

    Reinitialized database migrations for column name change.

--- bangazon/api/Migrations/20180130145049_InitialDBCreation.Designer.cs
+++ bangazon/api/Migrations/20180130145049_InitialDBCreation.Designer.cs
@@ -11,7 +11,7 @@ using System;
 namespace api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20180129160615_InitialDBCreation")]
+    [Migration("20180130145049_InitialDBCreation")]
     partial class InitialDBCreation
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
