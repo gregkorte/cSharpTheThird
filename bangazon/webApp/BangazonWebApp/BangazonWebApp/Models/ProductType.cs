d69b7f75e7754145766ba5280bcb1b97631c5655
Greg Korte <gregmkorte@gmail.com>
2018-02-21 08:22:53 -0600

    Added product and product type models.

---
+++ bangazon/webApp/BangazonWebApp/BangazonWebApp/Models/ProductType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWebApp.Models
+{
+    public class ProductType
+    {
+        [Key]
+        public int ProductTypeId { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [Display(Name="Category")]
+        public string Label { get; set; }
+
+        [NotMapped]
+        public int Quantity { get; set; }
+        public ICollection<Product> Products;
+    }
+}
