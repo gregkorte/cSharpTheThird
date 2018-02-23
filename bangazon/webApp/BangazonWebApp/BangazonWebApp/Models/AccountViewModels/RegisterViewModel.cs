133ccbbaba9d6919d76f415f8991c731186737a6
Greg Korte <gregmkorte@gmail.com>
2018-02-23 09:18:16 -0600

    Implemented registering users.

--- bangazon/webApp/BangazonWebApp/BangazonWebApp/Models/AccountViewModels/RegisterViewModel.cs
+++ bangazon/webApp/BangazonWebApp/BangazonWebApp/Models/AccountViewModels/RegisterViewModel.cs
@@ -9,6 +9,28 @@ namespace BangazonWebApp.Models.AccountViewModels
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        public string StreetAddress { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Required]
+        [Display(Name = "Zip Code")]
+        public string ZipCode { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
