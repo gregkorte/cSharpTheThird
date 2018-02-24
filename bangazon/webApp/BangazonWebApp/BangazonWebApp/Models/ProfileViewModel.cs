ebc58df1c135c2ef123e6ed8476d5cf404ddbfea
Greg Korte <gregmkorte@gmail.com>
2018-02-24 11:20:22 -0600

    Moved profile view model.

---
+++ bangazon/webApp/BangazonWebApp/BangazonWebApp/Models/ProfileViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWebApp.Models.ManageViewModels
+{
+    public class ProfileViewModel
+    {
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Address")]
+        public string StreetAddress { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        [Display(Name = "Zip Code")]
+        public string ZipCode { get; set; }
+
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        [Display(Name = "Phone number")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Payment Types")]
+        public ICollection<PaymentType> PaymentTypes { get; set; }
+
+        [Display(Name = "Order History")]
+        public ICollection<Order> Orders { get; set; }
+    }
+}
