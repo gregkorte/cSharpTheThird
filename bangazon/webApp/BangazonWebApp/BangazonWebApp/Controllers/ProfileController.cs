d58d66242d316666f143e48003739badaaef0017
Greg Korte <gregmkorte@gmail.com>
2018-02-25 10:26:27 -0600

    Added account index and edit views. Edit not working. Getting errors referring to profile nav page method.

---
+++ bangazon/webApp/BangazonWebApp/BangazonWebApp/Controllers/ProfileController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonWebApp.Data;
+using BangazonWebApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BangazonWebApp.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ProfileController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        // GET: Profile
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                throw new ApplicationException($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            ProfileViewModel model = new ProfileViewModel();
+
+            model.FirstName = user.FirstName;
+            model.LastName = user.LastName;
+            model.StreetAddress = user.StreetAddress;
+            model.City = user.City;
+            model.State = user.State;
+            model.ZipCode = user.ZipCode;
+            model.PhoneNumber = user.PhoneNumber;
+            StatusMessage = StatusMessage;
+
+            return View(model);
+        }
+
+        // GET: Profile/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Profile/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Profile/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Profile/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                throw new ApplicationException($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            ProfileViewModel model = new ProfileViewModel();
+
+            model.FirstName = user.FirstName;
+            model.LastName = user.LastName;
+            model.StreetAddress = user.StreetAddress;
+            model.City = user.City;
+            model.State = user.State;
+            model.ZipCode = user.ZipCode;
+            model.PhoneNumber = user.PhoneNumber;
+
+            return View(model);
+        }
+
+        // POST: Profile/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                StatusMessage = "Your profile has been updated.";
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Profile/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Profile/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
